575fa07e3f5c8f7fadfc474f2dbbe777989f79d5
beketov-dmitry <raff2004bd@gmail.com>
2023-01-11 22:37:01 +0300

    add fileUploading

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,6 @@
-import Archivates.ZipArchivate;
-import Encryptions.Calculate;
-import Encryptions.Encryptor;
 import Readers.ExpressionContainer;
-import Readers.JsonFileReader;
 import Readers.XmlFileReader;
-import org.mariuszgromada.math.mxparser.Expression;
-
-import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static void main(String[] args) {
